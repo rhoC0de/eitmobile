7b379ae7c6a77b09b2227eebc20cd2a274d95401
Rhoc0de <oreoluwaakanni@outlook.com>
2018-09-24 02:00:27 +0000

    creating form

--- eitmobile/imports/ui/body.js
+++ eitmobile/imports/ui/body.js
@@ -7,6 +7,35 @@ import './body.html';
  
 Template.body.helpers({
     eits() {
-        return Eits.find({});
+       // Show newest tasks at the top
+    return Eits.find({}, { sort: { createdAt: -1 } });
       },
 });
+Template.body.events({
+    'submit .new-eit'(event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+   
+      // Get value from form element
+    const target = event.target;
+    const firstname = target.firstname.value;
+    const surname = target.surname.value;
+    const gender = target.gender.value;
+    const dob = target.dob.value;
+   
+      // Insert a task into the collection
+      Eits.insert({
+        firstname,
+        surname,
+        gender,
+        dob,
+        createdAt: new Date(), // current time
+    });
+   
+      // Clear form
+    target.firstname.value = '';
+    target.surname.value = '';
+    target.gender.value = '';
+    target.dob.value = '';
+    },
+  });
